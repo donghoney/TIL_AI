2ad2d0361beafc3e27c517c2a8d04fb12fc86348
glee1228 <glee1228@users.noreply.github.com>
2019-03-16 16:36:26 +0900

    Update molecule.py

--- ML_basic/molecule.py
+++ ML_basic/molecule.py
@@ -5,20 +5,48 @@ def isNumber(x):
     except ValueError:
         return False
 
-def make_sign(a,b):
-    sign_dict={}
-
-    #print(lower_index)
-    #for i in range(0,len(a)):
-
 def molecule(letters):
     upper=[ord('A')<=ord(x) and ord(x) <=ord('Z') for x in letters]
     lower=[ord('a')<=ord(x) and ord(x) <=ord('b') for x in letters]
     number=[isNumber(x) for x in letters]
 
-    print(upper)
-    print(lower)
-    print(number)
-    result=make_sign(upper,lower)
-    print(result)
-molecule('C2O')
+    #for i in upper:
+
+    #print(result)
+molecule('CO12')
+
+def molecule2(letters):
+    string = letters
+    ans = string[0]
+    numlist = []
+    for idx, char in enumerate(string[1:]):
+        print(idx,char)
+        print(string[idx],char)
+        if string[idx].islower() and char.isupper():
+            ans += '-'
+        elif string[idx].isupper() and char.isupper():
+            ans += '-'
+        elif string[idx].isupper() and char.isnumeric():
+            ans += '-'
+        elif char.isnumeric():
+            numlist.append(char)
+        ans += char
+    print(ans)
+    anslst = ans.split('-')[:-1]
+    numlist = [i for i in ans.split('-')[-1]]
+    print(anslst)
+    print(numlist)
+    if len(anslst) != len(numlist):
+        print('error')
+    else:
+        lst = list(zip(anslst, numlist))
+        lst2 = []
+        for i in lst:
+            lst2.append(i[0] + i[1])
+        sum = ''
+        for i in lst2:
+            sum += i
+        sum = sum.replace('1', '')
+        print(sum)
+
+molecule2('COACl1234')
