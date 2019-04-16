633b40eb3c8f0f1b0c83d9b2daa0bda9a3645dfc
glee1228 <glee1228@users.noreply.github.com>
2019-04-16 13:18:37 +0900

    Update NetPresentValue.py

--- AI_Trader/DecisionAnalysis/NetPresentValue.py
+++ AI_Trader/DecisionAnalysis/NetPresentValue.py
@@ -1,27 +1,30 @@
 import numpy as np
 
-def npv(fv,r,n):
-    pv = 0
-    for i in range(1,n):
-        pv+=fv*(1/pow((1+r),i))
+def pv(fv,r):
+    pv=fv*(1/pow((1+r)))
     return pv
 
+def npv(fv_list,r):
+    npv=0
+    for fv in fv_list:
+        npv+=pv(fv,r)
+    return npv
+
+print('PV of Constant Annuity (Equal Amount starts at 𝒕 = 𝟏 for 𝑵 periods)',str(90/0.05))
+print('NPV {}'.format(npv(90,0.05,10000)))
 
 npv_profit =0
 
-num_list = [i for i in range(20,100)]
+fv_90_list = [90 for i in range(0,1000)]
 
-for i in num_list:
-    total_profit_pv = npv(90,0.05,i)-(90/(1+0.05))
-    #print(total_profit_pv)
-    npv_profit = total_profit_pv- npv(200,0.05,3)
-    #print(npv_profit)
-    npv_profit -= 1000
-    #print(npv_profit)
-    if npv_profit>0:
-        print(str(i)+'년째 부터 이익이 +입니다.')
-        print('npv : {}'.format(npv_profit))
-        break
-    else:
-        total_profit=0
-        npv_profit=0
+for fv in fv_90_list:
+    total_profit_pv = npv(fv,0.05)-(90/(1+0.05))
+    print(total_profit_pv)
+
+if npv_profit>0:
+    print(str(i)+'년째 부터 이익이 +입니다.')
+    print('npv : {}'.format(npv_profit))
+    break
+else:
+    total_profit=0
+    npv_profit=0
